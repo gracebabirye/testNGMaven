a33722a644dc79be4800802c1ecacf8e1793203e
gracebabirye <grycbabie@gmail.com>
2020-10-27 09:42:06 +0000

    ffffd

--- src/test/java/com/maven/testNGMaven/TestNgMavenExampleTest.java
+++ src/test/java/com/maven/testNGMaven/TestNgMavenExampleTest.java
@@ -4,15 +4,26 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class TestNgMavenExampleTest {
- 
-    @Test
-    public void exampleOfTestNgMaven() {
-        System.out.println("This is TestNG-Maven Example");
-    }
-    
-    @Test
-    public void testAdd() {
-       String str = "TestNG is working fine";
-       assertEquals("TestNG is working fine", str);
-    }
+
+	@Test
+	public void exampleOfTestNgMaven() {
+		System.out.println("This is TestNG-Maven Example");
+	}
+
+	@Test
+	public void testAdd() {
+		String str = "TestNG is working fine";
+		assertEquals("TestNG is working fine", str);
+	}
+
+	@Test
+	public void LoginTest() {
+		System.out.println("Successfully Logged In");
+	}
+
+	@Test
+	public void LogoutTest() {
+		System.out.println("Successfully Logged Out");
+	}
+
 }
